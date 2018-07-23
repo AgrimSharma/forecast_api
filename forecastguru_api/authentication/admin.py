ce74db1a7b4834f350ce8615ffcf0ee086ffba27
agrim <agrim.sharma@sirez.com>
2018-07-23 17:55:01 +0530

    updated 20180723

---
+++ forecastguru_api/authentication/admin.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from .models import *
+from django.contrib import admin
+
+
+class AuthenticationAdmin(admin.ModelAdmin):
+    list_display = ['email', 'facebook_id', 'last_login']
+
+
+class DailyPointsFreeAdmin(admin.ModelAdmin):
+    list_display = ['days', 'points']
+
+
+class JoiningBonusAdmin(admin.ModelAdmin):
+    pass
+
+
+class ForecastAdmin(admin.ModelAdmin):
+    list_display = ["forecast_heading", "forecast_category", "forecast_sub_category",
+                    "forecast_user", "forecast_status", "forecast_expire"]
+
+    def forecast_sub_category(self, obj):
+        return obj.sub_category.name
+
+    def forecast_category(self, obj):
+        return obj.category.name
+
+    def forecast_heading(self, obj):
+        return obj.heading
+
+    def forecast_user(self, obj):
+        return obj.user.email
+
+    def forecast_status(self, obj):
+        return obj.status.name
+
+    def forecast_expire(self, obj):
+        return obj.expire
+
+
+class StatusAdmin(admin.ModelAdmin):
+    pass
+
+
+class ApprovedAdmin(admin.ModelAdmin):
+    pass
+
+
+class WinningAdmin(admin.ModelAdmin):
+    pass
+
+
+class PrivateAdmin(admin.ModelAdmin):
+    pass
+
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class SubCatInline(admin.TabularInline):
+    model = SubCategory
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', "identifier"]
+    ordering = ('identifier',)
+    inlines = (SubCatInline,)
+
+
+class BettingAdmin(admin.ModelAdmin):
+    list_display = ["user_email", 'forecast_heading', "forecast_category", "forecast_sub_category", "bet_for", "bet_against"]
+
+    def user_email(self, obj):
+        return obj.users.email
+
+    def forecast_heading(self, obj):
+        return obj.forecast.heading
+
+    def forecast_category(self, obj):
+        return obj.forecast.category.name
+
+    def forecast_sub_category(self, obj):
+        return obj.forecast.sub_category.name
+
+
+class ReferralCodeAdmin(admin.ModelAdmin):
+    list_display = ["user_email", 'referral_code']
+
+    def user_email(self, obj):
+        return obj.user_joined.email
+
+
+class ReferralPointsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Authentication, AuthenticationAdmin)
+admin.site.register(ReferralCodePoints, ReferralPointsAdmin)
+admin.site.register(DailyFreePoints, DailyPointsFreeAdmin)
+admin.site.register(ReferralCodeRegistered, ReferralCodeAdmin)
+admin.site.register(Betting, BettingAdmin)
+admin.site.register(JoiningPoints, JoiningBonusAdmin)
+admin.site.register(ForeCast, ForecastAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Private, PrivateAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(Winning, WinningAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Approved, ApprovedAdmin)
+
+
+admin.site.site_title = 'ForeCast Guru'
+admin.site.site_header = 'ForeCast Guru'
+admin.site.index_title = 'Dashboard'
---
+++ forecastguru_api/authentication/serailizers.py
@@ -0,0 +1,103 @@
+from rest_framework import serializers
+from .models import *
+import string, random
+import re
+
+
+def id_generator(name):
+    r = re.compile(r"\s+", re.MULTILINE)
+    return r.sub("", str(name)).capitalize() + str(random.randrange(1111, 9999))
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Authentication
+        fields = ["id", "facebook_id", "full_name", 'gender', "email", "mobile"]
+
+    def create(self, validated_data):
+        auth = Authentication.objects.create(**validated_data)
+        joining = JoiningPoints.objects.get(id=1).points
+        auth.joining_points = joining
+        auth.referral_code = id_generator(auth.full_name)
+        return auth
+
+
+class LastLoginSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField()
+    login_date = serializers.DateField()
+
+    class Meta:
+        model = Authentication
+        fields = ["user_id", "login_date"]
+
+
+class ForecastSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = ForeCast
+        fields = ["id", "category", "sub_category", 'user', "heading", "status",
+                  "tags","expire"]
+
+    def create(self, validated_data):
+        return ForeCast.objects.create(**validated_data)
+
+
+class GetForecastSerializers(serializers.ModelSerializer):
+    user_id = serializers.IntegerField()
+
+    class Meta:
+        model = ForeCast
+        fields = ["user_id"]
+
+
+class PlaceBetSerializers(serializers.ModelSerializer):
+    user_id = serializers.IntegerField()
+    forecast_id = serializers.IntegerField()
+    points = serializers.IntegerField()
+    bet_id = serializers.IntegerField()
+
+    class Meta:
+        model = ForeCast
+        fields = ["user_id", "forecast_id", "points", "bet_id"]
+
+
+class GetPlayedForecastSerializers(serializers.ModelSerializer):
+    user_id = serializers.IntegerField()
+
+    class Meta:
+        model = Betting
+        fields = ["user_id"]
+
+
+class ReferralCodeSerializer(serializers.ModelSerializer):
+    referral_code = serializers.CharField()
+    user_joined = serializers.IntegerField()
+
+    class Meta:
+        model = Betting
+        fields = ["referral_code", "user_joined"]
+
+
+class UserInterestSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField(required=True)
+    interest = serializers.CharField(required=True)
+
+    class Meta:
+        model = UserInterest
+        fields = ["user_id", "interest"]
+
+
+class ResultSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField()
+
+    class Meta:
+        model = ForeCast
+        fields = ["user_id"]
+
+
+class AuthenticationSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField()
+
+    class Meta:
+        model = Authentication
+        fields = ["user_id"]
