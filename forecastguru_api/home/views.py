5e9834216815eb4bf0cfd1288e66ecc963674f5a
agrim <agrim.sharma@sirez.com>
2018-08-01 16:15:20 +0530

    updated

--- forecastguru_api/home/views.py
+++ forecastguru_api/home/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
 def login(request):
-    return render(request, "home/index.html", {})
+    if request.method == "POST":
+        return HttpResponse(request.__dict__)
+    else:
+        return render(request, "home/index.html", {})
 
