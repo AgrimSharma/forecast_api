4850ae39262a2c9e8ca64ecdebe349da20d7b544
agrim <agrim.sharma@sirez.com>
2018-08-01 16:36:45 +0530

    updated

--- forecastguru_api/home/views.py
+++ forecastguru_api/home/views.py
@@ -10,7 +10,7 @@ def login(request):
     if request.method == "POST":
         # data = json.loads(request.POST['perms'])
         # return HttpResponse(json.dumps(data['authResponse']))
-        return HttpResponse(json.dumps(request.__dict))
+        return HttpResponse(json.dumps(request.__dict__))
     else:
         return render(request, "home/index.html", {})
 
