1ab01c09e6a324dcf41093ac0a7186203d6863f8
agrim <agrim.sharma@sirez.com>
2018-08-01 16:58:14 +0530

    updated

--- forecastguru_api/home/views.py
+++ forecastguru_api/home/views.py
@@ -3,14 +3,19 @@ from __future__ import unicode_literals
 import json
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from django.contrib.auth.models import User
 
 @csrf_exempt
 def login(request):
     if request.method == "POST":
-        # data = json.loads(request.POST['perms'])
-        # return HttpResponse(json.dumps(data['authResponse']))
-        return HttpResponse(json.dumps(request.POST))
+        userID = json.loads(request.POST['userID'])
+        try:
+            user = User.objects.get(username=userID)
+            return HttpResponse("User Already Exists")
+        except Exception:
+            user = User.objects.create(username=userID)
+            user.save()
+            return HttpResponse("User Registered")
     else:
         return render(request, "home/index.html", {})
 
