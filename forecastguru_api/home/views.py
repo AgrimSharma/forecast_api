1ac23e0cb3cf607a9487deee1a3e1a6c7975e592
agrim <agrim.sharma@sirez.com>
2018-08-01 16:35:47 +0530

    updated

--- forecastguru_api/home/views.py
+++ forecastguru_api/home/views.py
@@ -8,8 +8,9 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def login(request):
     if request.method == "POST":
-        data = json.loads(request.POST['perms'])
-        return HttpResponse(json.dumps(data['authResponse']))
+        # data = json.loads(request.POST['perms'])
+        # return HttpResponse(json.dumps(data['authResponse']))
+        return HttpResponse(json.dumps(request.__dict))
     else:
         return render(request, "home/index.html", {})
 
