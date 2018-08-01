47b1ba5c1fd0af41ed6932ae491458da7b258414
agrim <agrim.sharma@sirez.com>
2018-08-01 16:29:46 +0530

    updated

--- forecastguru_api/home/views.py
+++ forecastguru_api/home/views.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import json
 from django.shortcuts import render, HttpResponse
 
 
 def login(request):
     if request.method == "POST":
-        return HttpResponse(request.__dict__)
+        data = json.loads(request.POST['data'])
+        return HttpResponse(json.dumps(data['authResponse']))
     else:
         return render(request, "home/index.html", {})
 
